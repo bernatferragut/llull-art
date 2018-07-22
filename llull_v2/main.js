d99dec956d5bb69dc875234d9b3825d579f3408e
bernatferragut <elbernat@gmail.com>
2018-07-22 12:31:25 -0400

    Looking for perfect randomization

--- llull_v2/main.js
+++ llull_v2/main.js
@@ -5,6 +5,7 @@
 let app = new Vue({
   el: '#app',
   data: {
+    // divinities object
     divinities : [
       {
         letter : "B",
@@ -88,13 +89,48 @@ let app = new Vue({
         vice: "Inconstancy"
       }
     ],
-    firstLetter: 'A',
+    // random letters
+    numbersList: [1,2,3,4,5,6,7,8,9],
+    shuffledList: [],
+    fourSelected: [],
+    randLetter:'?',
+    firstLetter: 'B',
     secondLetter: 'B',
-    thirdLetter: 'C'
+    thirdLetter: 'B',
+    fourthLetter: 'K',
+    // random questions
+    question:'question',
+    subject: 'subject',
+    verb: 'verb'
+    
   },
-  methods: {
-    randLetter : function() {
-      return this.divinities[Math.floor(Math.random()*10)].letter;
+created: function() {
+  // pseudo-shuffle trick!
+  this.shuffledList = this.numbersList.sort(function(){
+    return 0.5-Math.random()
+  });
+  this.fourSelected = this.shuffledList.splice(0,4);
+  console.log(this.fourSelected);
+  this.randLetter = this.fourSelected[Math.floor(Math.random()*4)];
+  // console.log(this.randLetter);
+  let i = this.fourSelected.indexOf(this.randLetter);
+  console.log(i);
+  this.fourSelected.splice(i,1,'K');
+  console.log(this.fourSelected);
+
+  // console.log(this.fourSelected);
+  this.firstLetter  = this.divinities[this.fourSelected[0]].letter;
+  this.secondLetter = this.divinities[this.fourSelected[1]].letter;
+  this.thirdLetter  = this.divinities[this.fourSelected[2]].letter;
+  this.fourthLetter = this.divinities[this.fourSelected[3]].letter;
+},
+
+methods: {
+  shuffleArray: function (array){
+    for (let i = array.length - 1; i > 0; i--) {
+        const rand = Math.floor(Math.random() * (i + 1));
+        [array[i], array[rand]] = [array[rand], array[i]];
     }
-  }
+  },
+}
 });
